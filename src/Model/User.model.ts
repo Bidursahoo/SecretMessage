58d152936c88827654949279ce15e63e5d849a1c
BidurPinnacle <bidur@pinnacleconsultingllc.com>
2024-04-29 20:29:56 +0530

    Added files

---
+++ src/Model/User.model.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, Document } from "mongoose";
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    reqired: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  verifyCode: {
+    type: String,
+    required: true,
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: true,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    required: true,
+  },
+  message: {
+    type: [MessageSchema],
+    required: true,
+  },
+});
